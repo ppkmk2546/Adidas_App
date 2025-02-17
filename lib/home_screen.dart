938524b8a48e60d1f22effe7a285fd63c067b667
Punpichit Puapun <punpichit46@gmail.com>
2025-02-17 22:07:13 +0700

    add new source code fullproject

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,32 +1,108 @@
+import 'package:adidas/adidas_screen.dart';
+import 'package:adidas/favorite_screen.dart';
+import 'package:adidas/search_screen.dart';
+import 'package:adidas/shopping_screen.dart';
+import 'package:adidas/home_content_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State <HomeScreen> createState() {
+  State<StatefulWidget> createState() {
     return _HomeScreenState();
   }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedindex = 0;
+
+  final Map<int, Map<String, Widget>> _navigationMap = {
+    0: {"title": const Text("Home"), "screen": HomeContentScreen()},
+    1: {"title": const Text("Search"), "screen": const SearchScreen()},
+    2: {"title": const Text("Favorite"), "screen": const FavoriteScreen()},
+    3: {"title": const Text("Cart"), "screen": const ShoppingScreen()},
+    4: {"title": const Text("Adidas"), "screen": const AdidasScreen()},
+  };
+
+  void onItemTapped(int index) {
+    setState(() {
+      _selectedindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // เอา debug ออก
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SHOP', style: TextStyle(letterSpacing: 0.5),),// กำหนด space ของ text
-          backgroundColor: Colors.white,
-          actions: [
-            Container(
-              margin: EdgeInsets.only(right: 16),
-              child: Icon(Icons.person_2_outlined),
-            )
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          margin: const EdgeInsets.only(left: 0),
+          child: const Text(
+            "SHOP",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              letterSpacing: 15,
+            ),
+          ),
         ),
-        body: Center(
-          child: Text('Welcome to Adidas!'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 25),
+            child: const Icon(Icons.person_2_outlined),
+          )
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0),
+          child: Container(
+            color: const Color.fromARGB(255, 240, 240, 240),
+            height: 2.0,
+          ),
         ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: IndexedStack(
+          index: _selectedindex,
+          children:
+              _navigationMap.values.map((page) => page["screen"]!).toList(),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedindex,
+        onTap: onItemTapped,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/icons/logo.png", width: 40, height: 40),
+            label: "",
+            backgroundColor: Colors.white,
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "",
+            backgroundColor: Colors.white,
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border_outlined),
+            label: "",
+            backgroundColor: Colors.white,
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: "",
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon:
+                Image.asset("assets/icons/adiclub.png", width: 70, height: 70),
+            label: "",
+            backgroundColor: Colors.white,
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:adidas/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(HomeScreen());
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, // ปิด debug banner
+      home: HomeScreen(), // เรียกใช้ HomeScreen
+    );
+  }
 }
